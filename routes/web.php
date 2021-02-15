be076c067f0d5bc6c527a98e9d068eb288906576
nathanael <nathanael.bjorkgard@billes.se>
2021-02-15 21:25:27 +0100

    Rename route

--- routes/web.php
+++ routes/web.php
@@ -59,4 +59,4 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
 Route::get('/bookings/{identifier}', [ExternalBookingController::class, 'index'])->name('bookings.thanks');
 Route::post('/bookings/greeting/{booking}', [BookingsController::class, 'greeting'])->name('bookings.greeting');
-Route::get('/reset-password', [AuthenticationController::class, 'resetPassword'])->name('password.reset');
+Route::get('/reset-password', [AuthenticationController::class, 'resetPassword']);
